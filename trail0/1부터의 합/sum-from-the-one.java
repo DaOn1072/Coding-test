d5333bc9797d89e4c72cfa8b14dddb3a6824692a
이다온 <139777054+DaOn1072@users.noreply.github.com>
2026-08-08 19:30:57 +0900

    [Passed] 1부터의 합 (126ms, 10MB)

---
+++ trail0/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int hap = 0;
+
+        for(int i = 1; i <= 100; i++){
+            hap += i;
+            if(hap >= n){
+                System.out.println(i);
+                break;
+            }
+        }
+        
+
+    }
+}
