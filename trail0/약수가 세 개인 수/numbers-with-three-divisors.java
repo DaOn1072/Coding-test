f3083b9994b25e00aae5ebdabed99121abcd9c43
이다온 <139777054+DaOn1072@users.noreply.github.com>
2026-08-09 20:00:23 +0900

    [Passed] 약수가 세 개인 수 (141ms, 11MB)

---
+++ trail0/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt(), end = sc.nextInt();
+        int answer = 0;
+
+        for(int i = start; i <= end; i++){
+            int cnt = 0;
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0){
+                    cnt++;
+                }
+            }
+
+            if(cnt == 3){
+                answer++;
+            }
+        }
+        
+        System.out.println(answer);
+    }
+}
